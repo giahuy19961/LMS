7895655c979239f3eb71bf565556f08625262205
Viet Tung <laiviettung510@gmail.com>
2022-04-10 13:21:48 +0700

    first server init

---
+++ controller/auth.controller.js
@@ -0,0 +1,61 @@
+const bcrypt = require("bcryptjs");
+const db = require("../db/db.config");
+const jwt = require("jsonwebtoken");
+
+const loginUser = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password)
+    return res
+      .status(400)
+      .json({ success: false, message: "Username and/or password is missing" });
+  try {
+    db.query(
+      `SELECT * FROM mdl_user WHERE username="${username}"`,
+      function (error, results, fields) {
+        if (!!error) {
+          return res
+            .status(400)
+            .json({ success: false, message: "User not found" });
+        } else {
+          isValidPassword = bcrypt.compareSync(password, results[0].password);
+          if (!isValidPassword) {
+            return res
+              .status(401)
+              .json({ success: false, message: "Unauthorized" });
+          }
+          const accessToken = jwt.sign(
+            { user: results[0].username },
+            process.env.JWT_SECRET_KEY
+          );
+          return res.json({
+            success: true,
+            message: "Login success",
+
+            data: {
+              info: {
+                firstName: results[0].firstname,
+                lastName: results[0].lastname,
+                email: results[0].email,
+                phone: results[0].phone1 || results[0].phone2,
+                city: results[0].city,
+                country: results[0].country,
+                id: results[0].id,
+              },
+              token: accessToken,
+            },
+          });
+        }
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+module.exports = {
+  loginUser,
+};
---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+// const mysql = require("mysql");
+const db = require("./db/db.config");
+var _ = require("lodash");
+require("dotenv").config();
+const moment = require("moment");
+const route = require("./routes");
+const cors = require("cors");
+const { config } = require("dotenv");
+
+// const bcrypt = require("bcryptjs");
+
+// create connect my sql
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   port: "3306",
+//   user: "root",
+//   password: "",
+//   database: "mysql",
+// });
+
+// connect
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("MySql Connected");
+});
+
+const app = express();
+
+// app.get("/", function (req, res) {
+//   db.query("SELECT * FROM mdl_user", function (error, rows, fields) {
+//     if (!!error) {
+//       console.log(error);
+//     } else {
+//       let password =
+//         "$2y$10$7gahn6nh.06zdSp8dNQN8eyz9m/PC.POpb90C6jAzfI2GbwF9wodO";
+//       console.log(bcrypt.compareSync("@12071996Hh", password));
+//       let resData = _.map(rows, (item, index) => {
+//         return {
+//           ...item,
+//           timestart: moment(item.timestart)
+//             .local()
+//             .format("YYYY-MM-DD HH:mm:ss"),
+//         };
+//       });
+
+//       return res.json({
+//         status: 200,
+//         data: resData,
+//       });
+//     }
+//   });
+// });
+app.use(express.json());
+app.use(cors());
+route(app);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
